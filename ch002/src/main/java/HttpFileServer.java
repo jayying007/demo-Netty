b4386dced1a21850c94a6f1c6f0162d8dd3f3641
jayying <zjy_mc@163.com>
2021-02-02 11:44:13 +0000

    完成ch002文件服务器

---
+++ ch002/src/main/java/HttpFileServer.java
@@ -0,0 +1,59 @@
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpRequestDecoder;
+import io.netty.handler.codec.http.HttpResponseEncoder;
+import io.netty.handler.stream.ChunkedWriteHandler;
+
+/**
+ * @author jayying
+ * @since 2021/2/2
+ */
+public class HttpFileServer {
+    private final static String DEFAULT_URL = "/home/jayying";
+
+    public void run(final int port, final String url) {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast("http-decoder", new HttpRequestDecoder());
+                            ch.pipeline().addLast("http-aggregator", new HttpObjectAggregator(65536));
+                            ch.pipeline().addLast("http-encoder", new HttpResponseEncoder());
+                            ch.pipeline().addLast("http-chunked", new ChunkedWriteHandler());
+                            ch.pipeline().addLast("fileServerHandler", new HttpFileServerHandler());
+                        }
+                    });
+            ChannelFuture f = b.bind(port).sync();
+            System.out.println("文件服务器启动成功，地址 http://127.0.0.1:" + port + url);
+            f.channel().closeFuture().sync();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) {
+        int port = 8080;
+        String url = DEFAULT_URL;
+        if(args.length > 0) {
+            port = Integer.parseInt(args[0]);
+        }
+        if(args.length > 1) {
+            url = args[1];
+        }
+        new HttpFileServer().run(port, url);
+    }
+}
